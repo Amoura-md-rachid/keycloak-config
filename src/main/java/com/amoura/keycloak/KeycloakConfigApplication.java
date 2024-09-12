4c2002c3d4fac5e02722af6cd683458296475946
m-amoura <m-amoura@enageo.com>
2024-09-12 16:10:03 +0100

    first commit

---
+++ src/main/java/com/amoura/keycloak/KeycloakConfigApplication.java
@@ -0,0 +1,13 @@
+package com.amoura.keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amoura/keycloak/KeycloakConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amoura.keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
